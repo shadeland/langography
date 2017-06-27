1a09246fa0a7ea66cb5fb98e7e0dba17e2e22c96
Adel Shah <shadel.shsh@gmail.com>
2017-06-27 15:18:35 -0400

    refactor wordcloud to its class

--- app/components/App.js
+++ app/components/App.js
@@ -6,6 +6,40 @@ const ReactRouter = require ('react-router-dom')
 const Router = ReactRouter.BrowserRouter
 const Route = ReactRouter.Route
 
+
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    // console.log(this.state)
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 class App extends React.Component {
 	render(){
 		return (
@@ -14,6 +48,7 @@ class App extends React.Component {
 				
 				<div className= 'container'>
 					<Nav />
+					<NameForm />
 					<Route  path="/popular" component={Popular}/>
 					<Route  path="/" component={Home}/>
 				</div>
--- app/index.js
+++ app/index.js
@@ -6,58 +6,133 @@ require('./index.css')
 const App = require('./components/App')
 const APIADDRESS = 'http://localhost:3000/api/lan/fa/tr/hello'
 
+// class WordCloud extends React.Component{
 
-const makeWorldCloud = (wordlist) => {
-	const fill = d3.scaleOrdinal(d3.schemeCategory20);
+// 	constructor(props){
+// 		super(props)
+// 		this.state = {
+// 			text : props.text
+// 		}
+// 	}
+
+// 	render(){
+
+// 	}
 
 
-    var color = d3.scaleLinear()
-            .domain([0,1,2,3,4,5,6,10,15,20,100])
-            .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
 
-    let layout = cloud().size([800, 800])
-            .words([])
-            .padding(1)
-            .font("Impact")
-            .rotate(function() { return ~~(Math.random() * 2) * 90; })
-            .fontSize(function(d) { return d.size; })
-            .on("end", draw)
-    
-    d3.json(APIADDRESS, (err,data) => {
-    	console.log("hey")
-    	// data = d3.entries(data.rates)
-    	let words = data.map((d) => {
-    		return {text: d.tran,
-    				size: 10 + Math.random() * 90}
-    	})
-    	layout.words(words)
-    	layout.start();	
-    })  
-    
+// }
 
-    function draw(words) {
-		        console.log(words)
-		        d3.select("body").append("svg")
-		      .attr("width", layout.size()[0])
-		      .attr("height", layout.size()[1])
-		    .append("g")
-		      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-		    .selectAll("text")
-		      .data(words)
-		    .enter().append("text")
-		      .style("font-size", function(d) { return d.size + "px"; })
-		      .style("font-family", "Impact")
-		      .style("fill", function(d, i) { return fill(i); })
-		      .attr("text-anchor", "middle")
-		      .attr("transform", function(d) {
-		        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-		      })
-		      .text(function(d) { return d.text; });
-    }
+class WorldCloud {
+	constructor(opts){
+		this.text = opts.text || 'hello'
+		this.layout = {}
+		this.init()
+	}
 
+	init(wordlist) {
+		
+		const fill = d3.scaleOrdinal(d3.schemeCategory20);
+
+	    let layout = cloud().size([800, 800])
+	            .words([])
+	            .padding(1)
+	            .font("Impact")
+	            .rotate(function() { return ~~(Math.random() * 2) * 90; })
+	            .fontSize(function(d) { return d.size; })
+	            .on("end", this.draw.bind(this))
+	    
+	    d3.json(APIADDRESS, (err,data) => {
+	    	console.log("hey")
+	    	// data = d3.entries(data.rates)
+	    	let words = data.map((d) => {
+	    		return {text: d.tran,
+	    				size: 10 + Math.random() * 90}
+	    	})
+	    	layout.words(words)
+	    	layout.start();	
+	    })
+	    this.layout = layout  
+
+	}
+	draw(words) {
+		const fill = d3.scaleOrdinal(d3.schemeCategory20);
+		
+		d3.select("body").append("svg")
+	      .attr("width", this.layout.size()[0])
+	      .attr("height", this.layout.size()[1])
+	      .append("g")
+	      .attr("transform", "translate(" + this.layout.size()[0] / 2 + "," + this.layout.size()[1] / 2 + ")")
+	    .selectAll("text")
+	      .data(words)
+	    .enter().append("text")
+	      .style("font-size", function(d) { return d.size + "px"; })
+	      .style("font-family", "Impact")
+	      .style("fill", function(d, i) { return fill(i); })
+	      .attr("text-anchor", "middle")
+	      .attr("transform", function(d) {
+	        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+	      })
+	      .text(function(d) { return d.text; });
+
+	}
 }
 
-makeWorldCloud()
+const wordCloud = new WorldCloud({})
+
+// const makeWorldCloud = (wordlist) => {
+// 	const fill = d3.scaleOrdinal(d3.schemeCategory20);
+
+
+//     var color = d3.scaleLinear()
+//             .domain([0,1,2,3,4,5,6,10,15,20,100])
+//             .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
+
+//     let layout = cloud().size([800, 800])
+//             .words([])
+//             .padding(1)
+//             .font("Impact")
+//             .rotate(function() { return ~~(Math.random() * 2) * 90; })
+//             .fontSize(function(d) { return d.size; })
+//             .on("end", draw)
+    
+//     d3.json(APIADDRESS, (err,data) => {
+//     	console.log("hey")
+//     	// data = d3.entries(data.rates)
+//     	let words = data.map((d) => {
+//     		return {text: d.tran,
+//     				size: 10 + Math.random() * 90}
+//     	})
+//     	layout.words(words)
+//     	layout.start();	
+//     })  
+    
+
+//     function draw(words) {
+// 		        console.log(words)
+// 		        d3.select("body").append("svg")
+// 		      .attr("width", layout.size()[0])
+// 		      .attr("height", layout.size()[1])
+// 		    .append("g")
+// 		      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+// 		    .selectAll("text")
+// 		      .data(words)
+// 		    .enter().append("text")
+// 		      .style("font-size", function(d) { return d.size + "px"; })
+// 		      .style("font-family", "Impact")
+// 		      .style("fill", function(d, i) { return fill(i); })
+// 		      .attr("text-anchor", "middle")
+// 		      .attr("transform", function(d) {
+// 		        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+// 		      })
+// 		      .text(function(d) { return d.text; });
+//     }
+
+// }
+
+// makeWorldCloud()
+
+
 
 ReactDOM.render(
 	<App />,
